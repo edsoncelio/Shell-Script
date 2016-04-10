84665f95f2b3a6669d8e93731ba6a7c1f46ce70f
Edson <edson@fedora.private>
2016-04-10 11:39:50 -0400

    comeco admin_github

--- admin_github.sh
+++ admin_github.sh
@@ -1,2 +1,14 @@
 !#bin/bash
 
+#funciona com o git já instalado
+
+#configurar user e mail
+echo 'user : '
+read usuario
+
+echo 'mail : '
+read email
+
+git config --global user.name usuario
+git config --global user.email email
+
