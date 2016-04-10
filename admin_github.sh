212131527b2a4ee13df5f74dda6e86ce107a9327
Edson <edson@fedora.private>
2016-04-10 11:48:46 -0400

    melhoras

--- admin_github.sh
+++ admin_github.sh
@@ -12,3 +12,19 @@ read email
 git config --global user.name usuario
 git config --global user.email email
 
+#criar diretorio de trabalho
+mkdir ${HOME}/.work_git
+
+#entrar
+cd  ${HOME}/.work_git
+
+#iniciar o git
+git init
+
+#adicionar o repositorio, nesse caso usei o https, mas poderia ser ssh ^^
+
+echo 'caminho do repositorio HTTPS : '
+read caminho 
+
+git remote add  $caminho
+
